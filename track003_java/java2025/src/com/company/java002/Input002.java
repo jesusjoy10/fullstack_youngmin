558a008f453f72ab24c6296275019c491f034682
youngmin <dudals5292xn@naver.com>
2025-08-29 17:50:11 +0900

    20250829 마무리

---
+++ track003_java/java2025/src/com/company/java002/Input002.java
@@ -0,0 +1,18 @@
+package com.company.java002;
+
+import java.util.Scanner;
+
+public class Input002 {
+	public static void main(String[] args) {
+		int age = 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("당신의 나이를 입력하시오 >");
+		age = scanner.nextInt();
+		System.out.println("내 나이는"+ age + "살 입니다");
+		
+		
+	
+		
+	}
+
+}
