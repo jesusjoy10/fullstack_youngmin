e4925fe081543a38c3983fce3874cde1f20d5926
youngmin <jesusjoy10@naver.com>
2025-09-01 12:11:19 +0900

    hello

---
+++ track003_java/java2025/src/com/company/java001_ex/A001_ex.java
@@ -0,0 +1,8 @@
+package com.company.java001_ex;
+
+public class A001_ex {
+
+public static void main(String[] arg) {
+	System.out.println("Hello world!");
+}
+}
---
+++ track003_java/java2025/src/com/company/java001_ex/A002_ex.java
@@ -0,0 +1,12 @@
+package com.company.java001_ex;
+
+public class A002_ex {
+  public static void main(String[] arg) {
+	 System.out.println("X\nXY\nXYZ");
+	 System.out.printf("도시 : %s , 인구 : %d","인천" , 1000000);
+  }
+}
+<<<<<<< HEAD
+=======
+
+>>>>>>> 139c10a9102b23059ac5c5520135bed86c051b64
