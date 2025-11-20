744d991e5a26a4c7dce58a94c195096e80465846
jesusjoy10 <dudals5292xn@naver.com>
2025-11-20 18:22:52 +0900

    20251120

---
+++ fullstack_youngmin/track006_spring/Spring005_board/src/main/java/com/thejoa703/dto/Sboard1Dto.java
@@ -0,0 +1,22 @@
+package com.thejoa703.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sboard1Dto {
+	private int id;
+	private int appUserId;
+	private String btitle;
+	private String bcontent;
+	private String bpass;
+	private String file;
+	private int bhit;
+	private String bip;
+	private LocalDateTime createdAt;
+}
